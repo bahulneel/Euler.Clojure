6454f3f5127a1d14824dac4bdff2af7e791003a0
Phil Booth <pmbooth@gmail.com>
2012-09-09 12:43:24 +0100

    Solution #2.

---
+++ 00002/solution.clj
@@ -0,0 +1,8 @@
+(defn sum-even-fibonacci [first second limit total]
+  (if (<= second limit)
+    (let [new-total (if (= (rem second 2) 0) (+ total second) total)]
+      (sum-even-fibonacci second (+ first second) limit new-total))
+    total))
+
+(sum-even-fibonacci 0 1 4000000 0)
+
