58524fa3cfe0ab5247e887295c6e1d66ba8d373b
Phil Booth <pmbooth@gmail.com>
2012-09-10 23:01:00 +0100

    Use recur rather than explicit recursion.

--- 00002/solution.clj
+++ 00002/solution.clj
@@ -1,7 +1,7 @@
 (defn sum-even-fibonacci [first second limit total]
   (if (<= second limit)
     (let [new-total (if (= (rem second 2) 0) (+ total second) total)]
-      (sum-even-fibonacci second (+ first second) limit new-total))
+      (recur second (+ first second) limit new-total))
     total))
 
 (println (sum-even-fibonacci 0 1 4000000 0))
