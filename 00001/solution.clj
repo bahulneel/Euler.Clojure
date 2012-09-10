9023af6d84ced4507f53028995c5b7c374850cdc
Phil Booth <pmbooth@gmail.com>
2012-09-10 23:01:31 +0100

    Use recur rather than explicit recursion.

--- 00001/solution.clj
+++ 00001/solution.clj
@@ -1,7 +1,7 @@
 (defn sum-multiples [multiples candidate limit total]
   (if (< candidate limit)
     (let [new-total (if (some #(= (rem candidate %) 0) multiples) (+ candidate total) total)]
-      (sum-multiples multiples (+ candidate 1) limit new-total))
+      (recur multiples (+ candidate 1) limit new-total))
     total))
 
 (println (sum-multiples [3 5] 1 1000 0))
