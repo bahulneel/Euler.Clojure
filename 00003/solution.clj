920bd2cc56e5ad60c9bb34e8e3f310fa618e4881
Phil Booth <pmbooth@gmail.com>
2012-09-12 23:07:06 +0100

    Ditch the sieve of eratosthenes.

--- 00003/solution.clj
+++ 00003/solution.clj
@@ -1,28 +1,14 @@
-(defn update-prime-candidates [current step candidates]
-  (if (candidates current)
-    (recur (+ current step) step candidates)
-    (assoc candidates current step)))
+(defn is-factor? [product candidate]
+  (= (rem product candidate) 0))
 
-(defn get-next-prime [current candidates]
-  (let [step (candidates current)]
-    (let [next (+ current 2)]
-      (if (candidates current)
-        (recur next 
-          (update-prime-candidates (+ current step) step (dissoc candidates current)))
-        (cons next
-          (lazy-seq
-            (get-next-prime next
-              (assoc candidates (* current current) (* current 2)))))))))
+(defn get-largest-prime-factor [product candidate]
+  (if (> candidate (int (Math/sqrt product)))
+    product
+    (if (= product candidate)
+      product
+      (if (is-factor? product candidate)
+        (recur (/ product candidate) candidate)
+        (recur product (+ candidate 1))))))
 
-(defn get-primes []
-  (cons 2
-    (lazy-seq
-      (get-next-prime 3 {}))))
-
-(defn get-prime-factors [product]
-  (for [prime (get-primes)]
-    (when (= (rem product prime) 0)
-      prime)))
-
-(println (apply max (filter (comp not nil?) (get-prime-factors 600851475143))))
+(println (get-largest-prime-factor 600851475143 2))
 
