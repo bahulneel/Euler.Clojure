1e3555c76c44fa4211266a0bdafc6b0bb94b2ba2
Phil Booth <pmbooth@gmail.com>
2012-09-16 12:17:33 +0100

    Add leiningen dependencies.

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject euler "1.0.0-SNAPSHOT"
+  :description "Gradually working through the problems of Project Euler."
+  :dependencies [[org.clojure/clojure "1.4.0"][org.clojure/math.numeric-tower "0.0.1"]])
